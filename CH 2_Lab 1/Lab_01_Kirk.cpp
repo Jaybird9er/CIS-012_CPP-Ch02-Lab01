861034bc962c96bae34d7c15cd95259c6aa17b7e
Jamey Kirk <JameyKirk@gmail.com>
2022-01-01 19:48:22 -0800

    Commit After Completion

---
+++ CH 2_Lab 1/Lab_01_Kirk.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num1 = 125;
+    int num2 = 28;
+    int num3 = -25;
+    int average;
+    average = (num1 + num2 + num3) / 3;
+    cout << "The values for each variable are: " << endl;
+    cout << "num1: " <<  num1 << endl;
+    cout << "num2: " << num2 << endl;
+    cout << "num3: " << num3 << endl;
+    cout << "The average of the numbers is: " << average << endl;
+    return 0;
+}
